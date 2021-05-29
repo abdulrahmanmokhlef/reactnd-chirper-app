52a1e635aff8dfe199d346615375d35fdf866ed2
abdulrahmanmokhlef <abdulrahman.mokhlef@gmail.com.com>
2021-05-29 18:54:58 +0200

    add yarn

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,7 +1,7 @@
 import { getInitialData } from '../utils/api';
 import { receiveTweets } from'./tweets';
 import { receiveUsers }  from './users';
-import { authedUser, setAuthedUser } from './authedUser';
+import { setAuthedUser } from './authedUser';
 
 const AUTHED_ID = 'tylermcginnis'
 
